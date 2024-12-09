ae040a7daabe0e4c632bb00e4d3781065172bc77
Skyllerz-IT <dadivarie@gmail.com>
2024-12-09 09:06:09 +0100

    Initial Commit

---
+++ Gamey Ai Library/Gamey_Ai_LibraryApp.swift
@@ -0,0 +1,32 @@
+//
+//  Gamey_Ai_LibraryApp.swift
+//  Gamey Ai Library
+//
+//  Created by Davide Picentino on 09/12/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Gamey_Ai_LibraryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Gamey Ai LibraryTests/Gamey_Ai_LibraryTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gamey_Ai_LibraryTests.swift
+//  Gamey Ai LibraryTests
+//
+//  Created by Davide Picentino on 09/12/24.
+//
+
+import Testing
+@testable import Gamey_Ai_Library
+
+struct Gamey_Ai_LibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
